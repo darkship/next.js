e134346b790714e6cfc2a9bd5dd6e3f71ea45867
강동윤 (Donny) <kdy1997.dev@gmail.com>
2025-05-02 09:08:23 -0700

    fix

--- turbopack/crates/turbopack-ecmascript/src/lib.rs
+++ turbopack/crates/turbopack-ecmascript/src/lib.rs
@@ -870,7 +870,7 @@ impl EcmascriptModuleContentOptions {
                             .code_generation(
                                 **module_graph,
                                 **chunking_context,
-                                *module,
+                                **module,
                                 Some(**parsed),
                                 *remove_unused_exports,
                             )
